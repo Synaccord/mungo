631dc14a031378507e27ef2a56edbe16a6bc2c96
francois <francoisrvespa@gmail.com>
2016-01-12 13:56:34 +0700

    migrating to v2: migration model

---
+++ app/models/migration.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import Model          from '../lib/model';
+import Type           from '../lib/type';
+import Schema         from '../lib/schema';
+
+class Migration extends Model {
+
+  static version = 2
+
+  static collection = 'mungo_migrations'
+
+  static schema = {
+    collection  :   {
+      type      :   String,
+      required  :   true
+    },
+    version     :   {
+      type      :   Number,
+      required  :   true
+    },
+    remove      :   Object,
+    unset       :   new Schema({
+      fields    :   [String],
+      get       :   Object
+    }),
+    update      :   new Schema({
+      get       :   Object,
+      set       :   Object
+    })
+  }
+}
+
+export default Migration;
